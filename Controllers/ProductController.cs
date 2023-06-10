6d609bca4a0c18ad4c9968c15a5f921d03e85126
Fasasi Qasim <fasaqa1000@gmail.com>
2023-06-10 12:24:27 +0100

    added a Get Request for all Products

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using MerchantApi.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MerchantApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpPost]
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(_db.Products);
+        }
+    }
+}
