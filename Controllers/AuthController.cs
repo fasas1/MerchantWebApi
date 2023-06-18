cdc9488ca536c9e001531e9bd3f0adc976d8b7e0
Fasasi Qasim <fasaqa1000@gmail.com>
2023-06-18 17:10:46 +0100

    added AuthTest controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
-
 using System.IdentityModel.Tokens.Jwt;
 using System.Net;
 using System.Security.Claims;
---
+++ Controllers/AuthTestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MerchantApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthTestController : ControllerBase
+    {
+        [HttpGet]
+         public async Task<ActionResult<string>> GetSomething()
+        {
+            return "You are authenticated!";
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<string>> GetSomething(int id)
+        {
+            return "You are authorized with the role of admin";
+        }
+    }
+}
