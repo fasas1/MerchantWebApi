55e60b44d82eb4829b24c26297b760c7a6edb5fc
Fasasi Qasim <fasaqa1000@gmail.com>
2023-07-03 17:05:04 +0100

    added shopping cart controller

---
+++ Controllers/ShoppingCartController.cs
@@ -0,0 +1,33 @@
+﻿using MerchantApi.Data;
+using MerchantApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MerchantApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+        public ShoppingCartController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new();
+        }
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> AddOrUpdateItemInCart(string userId, int productId, int updateQuantityBy)
+        {
+            ShoppingCart shoppingCart = _db.ShoppingCarts.FirstOrDefault(u => u.UserId == userId);
+            Product product = _db.Products.FirstOrDefault(u => u.Id == productId);
+            if (shoppingCart == null) {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+
+        }
+    }
+}
