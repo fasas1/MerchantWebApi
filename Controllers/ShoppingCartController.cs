5a0f1a83b5d84d40f1591631682d14f7faf3e481
Fasasi Qasim <fasaqa1000@gmail.com>
2023-07-05 02:03:17 +0100

    cartitem logic

--- Controllers/ShoppingCartController.cs
+++ Controllers/ShoppingCartController.cs
@@ -2,6 +2,7 @@
 using MerchantApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Net;
 
 namespace MerchantApi.Controllers
@@ -20,7 +21,7 @@ namespace MerchantApi.Controllers
         [HttpPost]
         public async Task<ActionResult<ApiResponse>> AddOrUpdateItemInCart(string userId, int productId, int updateQuantityBy)
         {
-            ShoppingCart shoppingCart = _db.ShoppingCarts.FirstOrDefault(u => u.UserId == userId);
+            ShoppingCart shoppingCart = _db.ShoppingCarts.Include(u => u.CartItems).FirstOrDefault(u => u.UserId == userId);
             Product product = _db.Products.FirstOrDefault(u => u.Id == productId);
             if (product == null) {
                 _response.StatusCode = HttpStatusCode.BadRequest;
@@ -28,8 +29,8 @@ namespace MerchantApi.Controllers
                 return BadRequest(_response);
             }
             if (shoppingCart == null && updateQuantityBy > 0)
-            { 
-               // Create Shipping Cart & Cart Item
+            {
+                // Create Shipping Cart & Cart Item
                 ShoppingCart newCart = new() { UserId = userId };
                 _db.ShoppingCarts.Add(newCart);
                 _db.SaveChanges();
@@ -47,6 +48,42 @@ namespace MerchantApi.Controllers
             else
             {
                 //shoppig cart exist 
+                CartItem cartItemInCart = shoppingCart.CartItems.FirstOrDefault(u => u.ProductId == productId);
+                if (cartItemInCart == null)
+                {
+                    // Item does not exist in shopping cart
+                    CartItem newCartItem = new()
+                    {
+                        ProductId = productId,
+                        Quantity = updateQuantityBy,
+                        ShoppingCartId = shoppingCart.Id,
+                        Product = null
+                    };
+                    _db.CartItems.Add(newCartItem);
+                    _db.SaveChanges();
+
+                }
+                else
+                {
+                    // Item already exist in the shopping cart and we have to update quantity
+                    int newQuantity = cartItemInCart.Quantity + updateQuantityBy;
+                     if(updateQuantityBy == 0 || updateQuantityBy <= 0)
+                    {
+                        // remove cart item from cart and if it is the only item then remove cart
+                        _db.CartItems.Remove(cartItemInCart);
+                        if(shoppingCart.CartItems.Count() == 1)
+                        {
+                            _db.ShoppingCarts.Remove(shoppingCart);
+                        }
+                        _db.SaveChanges();
+                    }
+                    else
+                    {
+                        //update quantity
+                        cartItemInCart.Quantity = newQuantity;
+                    }
+                    _db.SaveChanges();
+                }
             }
             return _response;
         }
