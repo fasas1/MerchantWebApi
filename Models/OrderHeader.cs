6a85ff9a67626cd29aa80e0f6cc56752d5efae1f
Fasasi Qasim <fasaqa1000@gmail.com>
2023-07-10 11:52:33 +0100

    added order header model

---
+++ Models/OrderHeader.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MerchantApi.Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int OrderHeaderId { get; set; }
+        [Required]
+        public string PickUpName { get; set; }
+        [Required]
+        public string PickUpPhoneNumber { get; set; }
+        [Required]
+        public string PickUpEmail { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser User { get; set; }
+        public double OrderTotal { get; set; }
+
+
+        public DateTime OrderDate { get; set; }
+        public string PaystackPaymentIntentId { get; set; }
+        public string Status { get; set; }
+        public int TotalItems { get; set; }
+
+
+    //    public IEnumerable<OrderDetails> OrderDetails { get; set; }
+    //}
+}
+}
